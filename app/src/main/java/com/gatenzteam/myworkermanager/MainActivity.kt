3176a2610e1ec1cebf0ac6b80985a760b2ff43aa
Purwa Adnyana <guspurwa123@gmail.com>
2023-09-22 10:02:46 +0800

    Memulai Latihan Work Manager untuk Background Scheduler

---
+++ app/src/main/java/com/gatenzteam/myworkermanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gatenzteam.myworkermanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
